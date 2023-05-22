c78577b949af0a5d04b33df06dd5ffcb948c5575
Erdinç Dursun <113687007+erdincdursun@users.noreply.github.com>
2023-05-22 22:26:37 +0300

    GetRequest

---
+++ Chapter_03/bookDemo/Controllers/BooksController.cs
@@ -0,0 +1,33 @@
+﻿using bookDemo.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bookDemo.Controllers
+{
+    [Route("api/books")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            var books = ApplicationContext.Books;
+            return Ok(books);
+
+        }
+
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetOneBook([FromRoute(Name ="id")]int id)
+        {
+            var book = ApplicationContext
+                .Books
+                .Where(b=>b.Id.Equals(id))
+                .SingleOrDefault();
+            if (book == null)
+                return NotFound(); //404
+            return Ok(book);
+
+        }
+    }
+}
